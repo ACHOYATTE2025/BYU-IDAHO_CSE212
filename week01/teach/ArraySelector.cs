b5df3ad7ab2be2e2f3625688c6cddc30155d3f7d
unknown <acho.quebec@gmail.com>
2025-09-04 16:54:53 +0000

    cse212

---
+++ week01/teach/ArraySelector.cs
@@ -0,0 +1,36 @@
+public static class ArraySelector
+{
+    public static void Run()
+    {
+        var l1 = new[] { 1, 2, 3, 4, 5 };
+        var l2 = new[] { 2, 4, 6, 8, 10};
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var intResult = ListSelector(l1, l2, select);
+        Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
+    }
+
+    private static int[] ListSelector(int[] list1, int[] list2, int[] select)
+    {
+        int lone = 0;
+        int ltwo = 0;
+        int [] result = new int [select.Length];
+        for(int i=0;i<select.Length;i++)
+        {
+            if (select[i] == 1)
+            {
+                
+                result[i] = list1[lone];
+                lone ++;
+               
+            }
+            else
+            {
+               
+                result[i] = list2[ltwo];
+                ltwo ++;
+                
+            }
+        }
+        return result;
+    }
+}
